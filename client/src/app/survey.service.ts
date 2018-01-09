bde8652a605609e568395844ab2f60db626997f2
Joe Calise <joe.calise@gmail.com>
2018-01-08 18:50:22 -0800

    update

--- client/src/app/survey.service.ts
+++ client/src/app/survey.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { BehaviorSubject } from 'rxjs/rx';
+import { BehaviorSubject } from 'Rxjs';
 import { Survey } from './survey';
 
 import { Observable } from 'rxjs/Observable';
