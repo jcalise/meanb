afafb917a48823291cc0a1d755b59dab8520f3bb
Joe Calise <joe.calise@gmail.com>
2018-01-08 18:44:37 -0800

    fix

--- client/src/app/survey.ts
+++ client/src/app/survey.ts
@@ -1,6 +1,6 @@
 export class Survey {
     constructor(
-        _id: string = '';
+        _id: string = '',
         public name: string = '',
         public question: string = '',
         public options: any[] = []
